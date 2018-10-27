6112024d88113a4e7098fd19b5bd391c85a647ba
WSLBJ <3218356596@qq.com>
2018-10-27 23:05:55 +0800

    new

---
+++ vue-quna/src/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+     <index-header></index-header>
+</template>
+
+<script>
+import IndexHeader from './header'
+export default {
+  name: 'index',
+  components: {
+    IndexHeader
+  }
+}
+</script>
+
+<style>
+ .header {
+    background: red;
+ }
+</style>
